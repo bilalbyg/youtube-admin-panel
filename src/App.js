8da7a54cc22a3b8a479acdd81aa9ccc7ffab3775
bilalbyg <bbeygo.71@gmail.com>
2023-09-04 16:29:31 +0300

    sidebar added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import React, { useState } from "react";
-// import Dashboard from "./scenes/dashboard"
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./scenes/dashboard";
 // import Team from "./scenes/team"
 // import Invoices from "./scenes/invoices"
 // import Contacts from "./scenes/global/contacts"
@@ -29,19 +30,19 @@ function App() {
         <Sidebar isSidebar={isSidebar} />
           <main className="content">
           <Topbar setIsSidebar={setIsSidebar} />
-            {/* <Routes>
+            <Routes>
               <Route path="/" element={<Dashboard />}/>
-              <Route path="/team" element={<Team />}/>
-              <Route path="/contacts" element={<Contacts />}/>
-              <Route path="/invoices" element={<Invoices />}/>
-              <Route path="/form" element={<Form />}/>
-              <Route path="/bar" element={<Bar />}/>
-              <Route path="/pie" element={<Pie />}/>
-              <Route path="/line" element={<Line />}/>
-              <Route path="/faq" element={<FAQ />}/>
-              <Route path="/geography" element={<Geography />}/>
-              <Route path="/calendar" element={<Calendar />}/>
-            </Routes> */}
+              {/* <Route path="/team" element={<Team />}/> */}
+              {/* <Route path="/contacts" element={<Contacts />}/> */}
+              {/* <Route path="/invoices" element={<Invoices />}/> */}
+              {/* <Route path="/form" element={<Form />}/> */}
+              {/* <Route path="/bar" element={<Bar />}/> */}
+              {/* <Route path="/pie" element={<Pie />}/> */}
+              {/* <Route path="/line" element={<Line />}/> */}
+              {/* <Route path="/faq" element={<FAQ />}/> */}
+              {/* <Route path="/geography" element={<Geography />}/> */}
+              {/* <Route path="/calendar" element={<Calendar />}/> */}
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
--- src/scenes/global/Sidebar.js
+++ src/scenes/global/Sidebar.js
@@ -1,21 +1,41 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
 import { Link } from "react-router-dom";
+import "react-pro-sidebar/dist/css/styles.css";
 import { tokens } from "../../theme";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
 import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
 import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import CalendayTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
 import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
-import PieChartOutlinedIcon from "@mui/icons-material/PieChartOutlined";
+import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
 import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
 
-export default function Sidebar() {
+const Item = ({ title, to, icon, selected, setSelected }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <MenuItem
+      active={selected === title}
+      style={{
+        color: colors.grey[100],
+      }}
+      onClick={() => setSelected(title)}
+      icon={icon}
+    >
+      <Typography>{title}</Typography>
+      <Link to={to} />
+    </MenuItem>
+  );
+};
+
+const Sidebar = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const [isCollapsed, setIsCollapsed] = useState(false);
@@ -24,10 +44,10 @@ export default function Sidebar() {
   return (
     <Box
       sx={{
-        "& .pro-side-inner": {
-          background: `${colors.primary[4]} !important`,
+        "& .pro-sidebar-inner": {
+          background: `${colors.primary[400]} !important`,
         },
-        "& .pro-side-wrapper": {
+        "& .pro-icon-wrapper": {
           backgroundColor: "transparent !important",
         },
         "& .pro-inner-item": {
@@ -36,7 +56,7 @@ export default function Sidebar() {
         "& .pro-inner-item:hover": {
           color: "#868dfb !important",
         },
-        "& .pro-inner-item.active": {
+        "& .pro-menu-item.active": {
           color: "#6870fa !important",
         },
       }}
@@ -60,7 +80,7 @@ export default function Sidebar() {
                 ml="15px"
               >
                 <Typography variant="h3" color={colors.grey[100]}>
-                  ADMINIS
+                  Admin Panel
                 </Typography>
                 <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
                   <MenuOutlinedIcon />
@@ -76,18 +96,130 @@ export default function Sidebar() {
                   alt="profile-user"
                   width="100px"
                   height="100px"
-                  src="https://w7.pngwing.com/pngs/552/711/png-transparent-elon-musk-tesla-motors-spacex-chief-executive-tesla-model-3-others-company-moustache-boring-company.png"
+                  src="https://pngimg.com/uploads/elon_musk/elon_musk_PNG2.png"
                   style={{ cursor: "pointer", borderRadius: "50%" }}
                 />
               </Box>
               <Box textAlign="center">
-                <Typography variant="h2" color={colors.grey[100]} fontWeight="bold" sx={{ m : "10px 0 0 0"}} >Administrator</Typography>
-                <Typography variant="h5" color={colors.greenAccent[500]}></Typography>
+                <Typography
+                  variant="h2"
+                  color={colors.grey[100]}
+                  fontWeight="bold"
+                  sx={{ m: "10px 0 0 0" }}
+                >
+                  Administrator
+                </Typography>
               </Box>
             </Box>
           )}
+
+          <Box paddingLeft={isCollapsed ? undefined : "10%"}>
+            <Item
+              title="Dashboard"
+              to="/"
+              icon={<HomeOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Typography
+              variant="h6"
+              color={colors.grey[300]}
+              sx={{ m: "15px 0 5px 20px" }}
+            >
+              Data
+            </Typography>
+            <Item
+              title="Manage Team"
+              to="/team"
+              icon={<PeopleOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Contacts Information"
+              to="/contacts"
+              icon={<ContactsOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Invoices Balances"
+              to="/invoices"
+              icon={<ReceiptOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Typography
+              variant="h6"
+              color={colors.grey[300]}
+              sx={{ m: "15px 0 5px 20px" }}
+            >
+              Pages
+            </Typography>
+            <Item
+              title="Profile Form"
+              to="/form"
+              icon={<PersonOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Calendar"
+              to="/calendar"
+              icon={<CalendarTodayOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="FAQ Page"
+              to="/faq"
+              icon={<HelpOutlineOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Typography
+              variant="h6"
+              color={colors.grey[300]}
+              sx={{ m: "15px 0 5px 20px" }}
+            >
+              Charts
+            </Typography>
+            <Item
+              title="Bar Chart"
+              to="/bar"
+              icon={<BarChartOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Pie Chart"
+              to="/pie"
+              icon={<PieChartOutlineOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Line Chart"
+              to="/line"
+              icon={<TimelineOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Geography Chart"
+              to="/geography"
+              icon={<MapOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+          </Box>
         </Menu>
       </ProSidebar>
     </Box>
   );
-}
+};
+
+export default Sidebar;
